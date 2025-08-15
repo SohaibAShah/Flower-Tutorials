3200ec301fe11c9e07292144649e4f7b26c30e30
SohaibAShah <asyedsohaib@gmail.com>
2025-08-15 23:26:24 +0800

    my_strategy.py updated

--- my-first-app/my_first_app/My_strategy.py
+++ my-first-app/my_first_app/My_strategy.py
@@ -1,19 +1,22 @@
 
 
 
+from logging import config
 from flwr.common import Metrics, FitRes, Parameters, parameters_to_ndarrays
 from flwr.server.client_proxy import ClientProxy
 from flwr.server.strategy import FedAvg
 from typing import List, Tuple
 from my_first_app.task import Net, set_weights
-import torch
+import torch, json
 
 
 class CustomFedAvg(FedAvg):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        
+
+        self.results_to_save = {}
+
     def aggregate_fit(self, 
                       server_round: int,
                       results: list[tuple[ClientProxy, FitRes]],
@@ -33,4 +36,23 @@ class CustomFedAvg(FedAvg):
         # Save global model in the standard PyTorch way
         torch.save(model.state_dict(), f"global_model_round_{server_round}.pth")
 
-        return parameters_aggredated, metrices_aggregated 
+        return parameters_aggredated, metrices_aggregated
+    
+
+    def evaluate(self, 
+                 server_round: int, 
+                 parameters: Parameters
+                 ) -> tuple[float, dict[str, bool | bytes | float | int, str]] | None:
+        # Custom evaluation logic
+        loss, metrics = super().evaluate(server_round, parameters)
+
+        my_results = {"loss": loss, **metrics}
+
+
+        self.results_to_save[server_round] = my_results
+
+        # Save metrics as json
+        with open("results.json", "w") as f:
+            json.dump(self.results_to_save, f, indent=4)
+
+        return loss, metrics
